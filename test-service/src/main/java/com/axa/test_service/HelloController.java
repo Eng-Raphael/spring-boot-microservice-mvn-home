ed14a39863ca98519a246fd7fa704f3929745fdf
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2025-09-30 16:07:22 +0300

    more clean

--- test-service/src/main/java/com/axa/test_service/HelloController.java
+++ test-service/src/main/java/com/axa/test_service/HelloController.java
@@ -1,40 +1,26 @@
 package com.axa.test_service;
 
 
+import com.axa.core_lib.http.UserServiceClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class HelloController {
 
-    private final WebClient userServiceWebClient;
+    private final UserServiceClient userServiceClient;
 
     @Autowired
-    public HelloController(WebClient userServiceWebClient) {
-        this.userServiceWebClient = userServiceWebClient;
+    public HelloController(UserServiceClient userServiceClient) {
+        this.userServiceClient = userServiceClient;
     }
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello(@RequestHeader("Authorization") String authHeader) {
-
-        ApiResponse<String> response = userServiceWebClient.get()
-                .uri("/validate")
-                .header("Authorization", authHeader)
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<ApiResponse<String>>() {})
-                .block();
-
-        if (response != null && "200".equals(response.getHeader())) {
-            String username = response.getBody();
-            return ResponseEntity.ok("Hello " + username + ", welcome to test-service!");
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid token");
-        }
+        String username = userServiceClient.validateToken(authHeader);
+        return ResponseEntity.ok("Hello " + username + ", welcome to test-service!");
     }
 }
