0c1efa540020842c80af155427fb6ef0ade22dca
Eng-Raphael <eng.rafael362.alfy@gmail.com>
2025-10-15 13:09:25 +0300

    removing old files

--- core-lib/src/main/java/com/axa/core_lib/exception/InvalidCredentialsException.java
+++
@@ -1,7 +0,0 @@
-package com.axa.core_lib.exception;
-
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException(String message) {
-        super(message);
-    }
-}
